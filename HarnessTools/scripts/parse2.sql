d4d5ab92ff91030e5261e40dc09445eba776a85e
Shepherd <Allen.Shepherd@diamondresorts.com>
2020-12-11 01:35:47 -0500

    updates

--- HarnessTools/scripts/parse2.sql
+++ HarnessTools/scripts/parse2.sql
@@ -15,38 +15,66 @@ DECLARE
 	length_from_i_to_j number :=0;
     v_occurence number :=1;
     begin
+      --break down inline clauses
+      v_i:=instr(sql_text,')',1);
+      v_k:=instr(substr(sql_text,1,v_i)  ,'(',-1);
+      while v_i > 0 loop
+        if instr(substr(sql_text,v_k, v_i-v_k),'SELECT ') >0 then
+          parse_tester(user_name, object_name,object_path, event_name, last_start_time,query_name, datasource, DB_USER, substr(sql_text, v_k+1,v_i-v_k-1), executions, v_depth+1);
+          sql_text := substr(sql_text,1,v_k-1) || ' {#QB_SEL$_' ||v_depth||'_'||v_occurence||'#} ' || substr(sql_text,v_i+1,length(sql_text)-v_i);
+          v_occurence:=v_occurence+1;
+        else
+          --dbms_output.put_line(sql_text);
+          sql_text := substr(sql_text,1,v_k-1) || '{FILLER_FUNCTION}' || substr(sql_text,v_i+1,length(sql_text)-v_i);
+          --dbms_output.put_line(sql_text);
+        end if;
+        v_i:=instr(sql_text,')',1);
+        v_k:=instr(substr(sql_text,1,v_i)  ,'(',-1);
+      end loop;
 
-      if instr(sql_text,' IN (',1)>0 then
-	      v_occurence:=0;
-	      v_i:=greatest( instr(sql_text,')',instr(sql_text,' IN (',1) ) ,length(sql_text));
-	      v_k:=instr(substr(sql_text,1,v_i)  ,'(',-1);
-	      while v_i > 0 loop
-	          sql_text := substr(sql_text,1,v_k-1) || '{ FILLER_IN_CONDITION }' || substr(sql_text,v_i+1,length(sql_text)-v_i);
-	        v_i:=instr(sql_text,')',instr(sql_text,' IN (',1) );
-	        v_k:=instr(substr(sql_text,1,v_i)  ,'(',-1);
-	        v_occurence:=v_occurence+1;
-	        exit when v_occurence =6;
-	      end loop;
-	      --dbms_output.put_line(sql_text);
-	  end if;
+
+      sql_text:=REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(sql_text, 'RIGHT JOIN', 'JCOND'),'RIGHT OUTER JOIN', 'JCOND'), 'LEFT JOIN', 'JCOND'), 'LEFT OUTER JOIN', 'JCOND'), 'FULL OUTER JOIN', 'JCOND'), 'INNER JOIN', 'JCOND'),'JOIN','JCOND');
+      sql_text:=REPLACE(REPLACE(REPLACE( sql_text, CHR(10) ), CHR(13) ,' '),CHR(9),' ');
+      sql_text:=REPLACE(REPLACE(replace(sql_text,'	',' '),',',','), '  ', ' ');
+      sql_text:=replace(replace(sql_text,')',' ) '),'(',' ( ');
+      sql_text:=REPLACE(REPLACE(replace(sql_text,'	',' '),',',','), '  ', ' '); --ran twice to ensure braces have spaces and being sensitive to 4k limit
+      sql_text:=replace(sql_text,'''''','**');
+      
+      
+      --This is mainly for SFS report --> Static Pool Defaults ACCT v2BC
+      --remove space between double quotes
 	  while instr(SQL_TEXT,'"',1,1)>0 loop
 	    v_i:=instr(SQL_TEXT,'"',1,1);
 	    v_k:=instr(SQL_TEXT,'"',1,2);
 	    SQL_TEXT:=substr(SQL_TEXT,1,v_i-1)||replace(substr(SQL_TEXT,v_i+1,v_k-v_i-1),' ','_')||substr(SQL_TEXT,v_k+1, length(SQL_TEXT));
 	  end loop;
-	  --dbms_output.put_line(sql_text);
-
-      sql_text:=REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(sql_text, 'RIGHT JOIN', 'JCOND'),'RIGHT OUTER JOIN', 'JCOND'), 'LEFT JOIN', 'JCOND'), 'LEFT OUTER JOIN', 'JCOND'), 'FULL OUTER JOIN', 'JCOND'), 'INNER JOIN', 'JCOND');
-      sql_text:=REPLACE(REPLACE( sql_text, CHR(10) ), CHR(13) ,' ');
-      sql_text:=REPLACE(REPLACE(replace(sql_text,'	',' '),',',' , '), '  ', ' ');
       
+      
+      --remove comments
+      v_i:=instr(sql_text,'''',1,1);
+      v_k:=instr(sql_text,'''',1,2);
+      while v_i > 0 loop
+        --dbms_output.put_line('------->'||substr(sql_text,1,v_i-1));
+        sql_text:=substr(sql_text,1,v_i-1)||'"comment_removed"'||substr(sql_text,v_k+1,length(sql_text)-v_i);
+        v_i:=instr(sql_text,'''',1,1);
+        v_k:=instr(sql_text,'''',1,2);
+      end loop;
 
+      --remove comment blocks
+      v_i:=instr(sql_text,'/*',1);
+      v_k:=instr(sql_text,'*/',1);
+      while v_i > 0 loop
+        sql_text:=substr(sql_text,1,v_i-1)||substr(sql_text,v_k+2,length(sql_text)-v_i);
+        v_i:=instr(sql_text,'/*',1);
+        v_k:=instr(sql_text,'*/',1);
+      end loop;
+      sql_text:=REPLACE(REPLACE(replace(sql_text,'	',' '),',',','), '  ', ' ');
+      --remove inline queries
+/*
       v_i:=instr(sql_text,')',1);
       v_k:=instr(substr(sql_text,1,v_i)  ,'(',-1);
       while v_i > 0 loop
         if instr(substr(sql_text,v_k, v_i-v_k),'SELECT ') >0 then
-          --dbms_output.put_line(substr(sql_text, v_k+1,v_i-v_k-1));
-          --parse_tester(substr(sql_text, v_k+1,v_i-v_k-1),v_depth+1, object_name);
           parse_tester(user_name, object_name,object_path, event_name, last_start_time,query_name, datasource, DB_USER, substr(sql_text, v_k+1,v_i-v_k-1), executions, v_depth+1);
           sql_text := substr(sql_text,1,v_k-1) || '{#QB_SEL$_' ||v_depth||'_'||v_occurence||'#}' || substr(sql_text,v_i+1,length(sql_text)-v_i);
           v_occurence:=v_occurence+1;
@@ -59,17 +87,27 @@ DECLARE
         v_k:=instr(substr(sql_text,1,v_i)  ,'(',-1);
       end loop;
       --dbms_output.put_line(sql_text);
+*/
 
 
-
+	  dbms_output.put_line(sql_text);
 	  select substr(sql_text, 1, instr(sql_text,' ')) command_type into var_command from dual;
 	  select substr(sql_text, instr(sql_text,' ')+1 , instr(sql_text,' FROM ', -1,1)-instr(sql_text,' ')-1) INTO var_columns from dual;
 	  if instr(sql_text,' WHERE ', 1,1)>0 then
   	    select substr(sql_text, instr(sql_text,' FROM ' , 1,1)+6 , instr(sql_text,' WHERE ', 1,1) - instr(sql_text,' FROM ', 1,1)-6 ) INTO var_temp_placeholder from dual;
 	    select substr(sql_text, instr(sql_text,' WHERE ', 1,1)+7)  INTO var_filters from dual;
 	  else -- ... check for group by, having, order by, union, union all, intersect, minus, and finally with.....
-	    v_i:=greatest(least(instr(sql_text,' JCOND ', 1,1),instr(sql_text,' WHERE ', 1,1),instr(sql_text,' GROUP BY ', 1,1), instr(sql_text,' ORDER BY ', 1,1)),length(sql_text));
-  	    select substr(sql_text, instr(sql_text,' FROM ' , 1,1)+6 , v_i - instr(sql_text,' FROM ', 1,1)-6 ) INTO var_temp_placeholder from dual;
+	    case when instr(sql_text,' GROUP BY ', 1,1)>0 then 
+	         v_i:=instr(sql_text,' GROUP BY ', 1,1);
+	         when instr(sql_text,' ORDER BY ', 1,1)>0 then 
+	         v_i:=instr(sql_text,' ORDER BY ', 1,1);
+	         else 
+	         v_i:=length(sql_text);
+	    end case;
+	    
+
+  	    select substr(sql_text, instr(sql_text,' FROM ' , 1,1)+6 , v_i - instr(sql_text,' FROM ', 1,1)-6) INTO var_temp_placeholder from dual;
+  	    --dbms_output.put_line('this is placeholder ---->'|| var_temp_placeholder);
   	    --select substr(sql_text, instr(sql_text,' WHERE ', 1,1)+7)  INTO var_filters from dual;
 	  end if;
 	  --dbms_output.put_line('THIS IS MY LIST OF TABLES ---------------------->'||var_temp_placeholder);
@@ -94,32 +132,31 @@ DECLARE
         exit when v_occurence >19;
       end loop;
       
- --     if instr(var_tables,',',1)=0 then
         if instr(var_tables,' ',2,1)=0 then 
-          dbms_output.put_line('insert into test_table (table_name) values ('''||var_tables ||''');');
+          dbms_output.put_line('insert into test_table1 (table_name) values ('''/*||db_user||'@'||datasource||' '*/||var_tables ||''');     ------->'|| object_name);
         else
-          var_temp_placeholder:=var_tables;
+          var_temp_placeholder:=var_tables; 
           v_i:=instr(var_temp_placeholder,',',1);
+          --dbms_output.put_line('this is tables      ---->'|| var_tables);
           while v_i>0 loop
-            dbms_output.put_line('insert into test_table (table_name) values ('''||substr(var_temp_placeholder,1,instr(var_temp_placeholder,' ',1,1)-1) ||''');');
+            dbms_output.put_line('insert into test_table2 (table_name) values ('''/*||db_user||'@'||datasource||' '*/||substr(var_temp_placeholder,1,instr(var_temp_placeholder,' ',1,1)-1) ||''');     ------->'|| object_name);
             var_temp_placeholder:=substr(var_temp_placeholder,v_i+2, length(var_temp_placeholder)-v_i);
             v_i:=instr(var_temp_placeholder,',',1);
           end loop;
           if instr(var_temp_placeholder,' ',1,1)=0 then 
-            dbms_output.put_line('insert into test_table (table_name) values ('''||var_temp_placeholder ||'''); 		--'||object_name);
+            dbms_output.put_line('insert into test_table3 (table_name) values ('''/*||db_user||'@'||datasource||' '*/|| var_temp_placeholder ||''');     ------->'|| object_name);
           else
             if instr(var_temp_placeholder,'"',1,1)=0 then
-			dbms_output.put_line('insert into test_table (table_name) values ('''||substr(var_temp_placeholder,1,instr(var_temp_placeholder,' ',1,1)-1) ||''');');
+			dbms_output.put_line('insert into test_table4 (table_name) values ('''/*||db_user||'@'||datasource||' '*/||substr(var_temp_placeholder,1,instr(var_temp_placeholder,' ',1,1)-1) ||''');     ------->'|| object_name);
 			else
 			 while instr(var_temp_placeholder,'"',1,1)>0 loop
 			   v_i:=instr(var_temp_placeholder,'"',1,1);
 			   v_k:=instr(var_temp_placeholder,'"',1,2);
 			   var_temp_placeholder:=substr(var_temp_placeholder,1,v_i-1)||replace(substr(var_temp_placeholder,v_i+1,v_k-v_i-1),' ','_')||substr(var_temp_placeholder,v_k+1, length(var_temp_placeholder));
 			 end loop;
-			 dbms_output.put_line('insert into test_table (table_name) values ('''||substr(var_temp_placeholder,1,instr(var_temp_placeholder,' ',1,1)-1) ||''');');
+			 dbms_output.put_line('insert into test_table5 (table_name) values ('''/*||db_user||'@'||datasource||' '*/||substr(var_temp_placeholder,1,instr(var_temp_placeholder,' ',1,1)-1) ||''');     ------->'|| object_name);
 			end if;
           end if;
-
         end if;
     
       --dbms_output.put_line('=========================================================');
@@ -131,7 +168,8 @@ DECLARE
       
 	end;
 BEGIN
-  
+  v1_sql_text:='USE DRINCCC SELECT  E.DEPARTMENT_DESCRIPTION, A.TEAM_DESC, C.ALIAS, D.ATLAS_USERNAME, CASE D.ACTIVE  WHEN 1 THEN "c"Y"c"  ELSE "c"N"c" END AS ACTIVE, B.DATE_END AS END_DATE FROM  LEADS.TEAM A JCOND  LEADS.DEPARTMENT_AGENT B ON A.TEAM_ID = B.TEAM_ID JCOND  LEADS.AGENTS C ON C.NOBLE_AGENT_ID = B.NOBLE_AGENT_ID JCOND VDW_AGENT D ON C.NOBLE_AGENT_ID = D.TSR JCOND LEADS.DEPARTMENT E ON
+A.DEPARTMENT_CODE = E.DEPARTMENT_CODE WHERE  C.ACTIVE = 1 AND B.DATE_END IS NULL AND {FILLER_FUNCTION} ORDER BY E.DEPARTMENT_DESCRIPTION, A.TEAM_DESC, C.ALIAS';
   for row in (
 				select  *
 				from
@@ -152,26 +190,108 @@ BEGIN
 				where a.user_id = b.user_id
 				and b.event_id = c.event_id
 				and d.event_id = b.event_id
-				and c.object_name  not in ('Authorized_Rep')
 				--and upper(query_string) not like '%PREMIER.%' and upper(query_string) not like '%DBO.%' and upper(query_string) not like'%"PREMIER".%' and upper(query_string) not like '%"DBO".%' 
-				and c.object_name not in ('CVV Tours','Agent Tour Grid','StatusReporting_TermModification','Confirmations','DTA','Hourly Stats','I3 Agent List','QA TM Audit','I3 Call Data','TPS V3 Main CAL','VUDOO Current',
-					'OTA Availability','CLUB Discount_Brad2-copy')
+				and c.object_name in ('I3 Agent List')
+				--and c.object_name in ('StatusReporting_TermModification','Confirmations','DTA','Hourly Stats','I3 Agent List','QA TM Audit','I3 Call Data','TPS V3 Main CAL','VUDOO Current', 'OTA Availability','CLUB Discount_Brad2-copy')
 				group by a.user_name, c.object_name, c.object_path, b.event_name, d.query_name, d.datasource, d.query_string, D.DB_USER
 				order by c.object_path, c.object_name 
-				) --where rownum<1000
+				) --where rownum=1
   			)
   loop
     --dbms_output.put_line(row.object_name);
     --dbms_output.put_line(row.query_string);
-    if /*upper(row.query_string) not like '%WITH % AS%' and*/ length(row.query_string)<3900 then
-      dbms_output.put_line('___________________________________________________________________________________');
+    if upper(row.query_string) not like '%WITH % AS%' and upper(row.query_string) not like '%OPENQUERY%' and length(row.query_string)<4000 then
+      --dbms_output.put_line('___________________________________________________________________________________');
       --dbms_output.put_line(row.query_string);
+      --dbms_output.put_line(row.db_user||'@'||row.datasource);
       --dbms_output.put_line(row.object_name);
-      --parse_tester(upper(replace(trim(row.query_string),'"','"')),0, row.object_name);
+      --dbms_output.put_line('___________________________________________________________________________________');
+      --dbms_output.put_line(v1_sql_text);
       parse_tester(row.user_name, row.object_name, row.object_path,row.event_name, row.last_start_time,row.query_name, row.datasource,row.DB_USER,upper(trim(row.query_string)), row.executions, 0);
+      --parse_tester(row.user_name, row.object_name, row.object_path,row.event_name, row.last_start_time,row.query_name, row.datasource,row.DB_USER,upper(trim(v1_sql_text)), row.executions, 0);
       --dbms_output.put_line('commit;');
     end if;
   end loop;
 END;
 /
 
+/* users with prefix
+$ cat output.txt | grep insert | awk -F '----' '{print $1 }' | grep -v "QB_SEL"| grep "\." |awk -F';' '{print $1}'| awk -F"'" '{print $2}' | awk -F'.' '{print $1}' | sort -u
+ATLAS
+CCI
+CONVERT
+DATAWH
+DBO
+EPMRAF
+HSI
+KMARCOS
+LBACSYS
+LEADS
+MDSYS
+NOETIX_NEW_SYS
+NORTRIDGE
+OTA_INTERFACE
+PAUDIT
+PREMIER
+SFS_DBO
+SQLPDBAV10
+WEBRES
+WEBUI
+WMSYS
+
+*/
+
+/* tables without a prefix entirely
+$ cat output.txt | grep -v '\.' | awk -F"'" '{print $2}' | awk -F'.' '{print $1}' | sort -u
+
+(SELECT
+{#QB_SEL$_0_1#}
+{#QB_SEL$_0_2#}
+{#QB_SEL$_0_3#}
+{#QB_SEL$_0_4#}
+{#QB_SEL$_0_5#}
+{#QB_SEL$_0_6#}
+{#QB_SEL$_0_7#}
+{#QB_SEL$_0_7#}DAYQRY
+{#QB_SEL$_0_8#}
+1
+AR_CLUB_GROUP
+CASE
+CONCAT{FILLER_FUNCTION}
+DUAL
+FDI_CHOICE
+LN_NLS_PAYMENT_EXCEPTIONS
+LN_NLS_PAYMENT_FILES
+LN_NLS_TASK_TERM_MOD_ELIGIBILILTY
+MAIN_QUERY
+NLS_LOAN_PAYMENT_METHOD
+NVL{FILLER_FUNCTION}
+P_BOX_PROGRAM,
+P_CLOSING_COST
+P_CLOSING_COST_TYPE
+P_CONTRACT
+P_CONTRACT_APPLY_TO
+P_CONTRACT_HISTORY
+P_CONTRACT_LEAD
+P_CONTRACT_LOAN
+P_CONTRACT_PAY_SCHEDULE
+P_CONTRACT_PURCHASE
+P_CONTRACT_TRADEOUT
+P_ITEM_MASTER
+P_MEMBER_CONTRACT
+P_TOUR_TYPE,
+PV_TOUR_STATUS,
+R_SERVICER
+S_MEMBERSHIP
+S_TRANSACTION
+V_NLS_LOANACCT
+V_NLS_LOANACCT_BATCH
+V_NLS_LOANACCT_COMMENTS
+V_NLS_LOANACCT_PAYMENT_HISTORY_DETAIL
+V_NLS_LOANACCT_PAYMENT_HISTORY_HEADER
+V_NLS_LOANACCT_STATUSES
+V_NLS_TASK
+V_NLS_TASK_WORKFLOW_DETAIL
+VDW_AGENT
+
+*/
